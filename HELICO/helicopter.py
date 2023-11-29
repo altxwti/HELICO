798236d6c7c52fb2ccefba596170e2d93925458e
altxwti <134724610+altxwti@users.noreply.github.com>
2023-11-29 20:27:16 +0300

    Add files via upload

---
+++ HELICO/helicopter.py
@@ -0,0 +1,38 @@
+from  utils import randcell
+import os
+
+class Helicopter:
+    def __init__(self, w, h):
+        rc = randcell(w, h)
+        rx, ry = rc[0], rc[1]
+        self.x = rx
+        self.y = ry
+        self.w = w
+        self.h = h
+        self.tank = 0
+        self.mxtank = 1
+        self.score = 0
+        self.lives = 20
+
+    def move(self, dx, dy):
+        nx, ny = dx + self.x, dy + self.y
+        if (nx >= 0 and ny >= 0 and nx < self.h and ny < self.w):
+            self.x, self.y = nx, ny
+
+    def print_stats(self):
+        print("⛲", self.tank, "/", self.mxtank, sep="", end=" | ")
+        print("🏆", self.score, end=" | ")
+        print("❤️", self.lives)
+
+    def game_over(self):
+        os.system("cls")
+        print("GAME OVER")
+        exit(0)
+
+    def export_data(self):
+        return {"score": self.score,
+                "lives": self.lives,
+                "x": self.x, "y": self.y,
+                "tank": self.tank,
+                "mxtank": self.mxtank}
+
